c9bec478519087b1c90639459e6fe14b9a163423
Michal Šára <mikesh.cz@gmail.com>
2025-11-16 19:55:53 +0100

    build_dmg script

---
+++ build_dmg/build_dmg.sh
@@ -0,0 +1,11 @@
+create-dmg \
+--filesystem APFS \
+--volname "Archive Quick Look" \
+--volicon "ArchiveQuickLook.icns" \
+--window-pos 200 120 \
+--window-size 600 400 \
+--icon-size 64 \
+--icon "ArchiveQuickLook.app" 175 190 \
+--app-drop-link 425 190 \
+"ArchiveQuickLook-1.2.0-macos-arm64.dmg" \
+"ArchiveQuickLook.app"
